cfe280aa12b024ab110877ff733615d8e7f8e8c7
Bruno Lopes <eusoubruno7@gmail.com>
2023-06-30 19:10:54 -0300

    refactor: update user module

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,52 +1,12 @@
-import { ConflictException, Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { hash } from 'bcryptjs';
+import { Injectable } from '@nestjs/common';
 import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
-  async create(createUserDto: CreateUserDto) {
-    const { email, name, password } = createUserDto;
 
-    const emailAlreadyInUse = await this.usersRepository.findUnique({
-      where: { email },
-      select: { id: true },
-    });
-
-    if (emailAlreadyInUse) {
-      throw new ConflictException('This email is already in use!');
-    }
-
-    const hashedPassword = await hash(password, 12);
-
-    const user = await this.usersRepository.create({
-      data: {
-        email,
-        name,
-        password: hashedPassword,
-        categories: {
-          createMany: {
-            data: [
-              { name: 'Salário', icon: 'salary', type: 'INCOME' },
-              { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
-              { name: 'Outro', icon: 'other', type: 'INCOME' },
-
-              { name: 'Casa', icon: 'home', type: 'EXPENSE' },
-              { name: 'Alimentação', icon: 'food', type: 'EXPENSE' },
-              { name: 'Educação', icon: 'education', type: 'EXPENSE' },
-              { name: 'Lazer', icon: 'fun', type: 'EXPENSE' },
-              { name: 'Mercado', icon: 'grocery', type: 'EXPENSE' },
-              { name: 'Roupas', icon: 'clothes', type: 'EXPENSE' },
-              { name: 'Transporte', icon: 'transport', type: 'EXPENSE' },
-              { name: 'Viagem', icon: 'travel', type: 'EXPENSE' },
-              { name: 'Outro', icon: 'other', type: 'EXPENSE' },
-            ],
-          },
-        },
-      },
-    });
-
+  async getUserById(id: string) {
+    const user = await this.usersRepository.findUnique({ where: { id } });
     return {
       name: user.name,
       email: user.email,
